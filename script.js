6a3f68491939a6ca62d5d182341b3276a83d0def
EmelieVilor <emelie.vilor@gmail.com>
2025-03-07 09:44:29 +0100

    Added functionality

--- script.js
+++ script.js
@@ -23,160 +23,119 @@ function themeSwitch() {
     console.log('You have clicked the button!');
 }
 
+//date in footer
+
+let date = new Date().toLocaleDateString("sv-SE");
+console.log(date);
+document.getElementById("date").innerHTML = `${date} &copy Emelie Nilsson Vilör`;
 
 
+//Json CV
 
-// Modals
-const modal = document.getElementById("Modal1");
-const btn = document.getElementById("modalBtn1");
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
-//test
-
-// const modeBtn = document.querySelector(".modeBtn");
-// let lightmode = localStorage.getItem(".lightmode");
-
-// const enableLightMode = () => {
-//     document.body.classList.add("lightmode");
-//     // toggleBtn.classList.remove("dark-mode-toggle");
-//     localStorage.setItem("lightmode", "enabled");
-// };
-
-// const disableLightMode = () => {
-//     document.body.classList.remove("lightmode");
-// //   toggleBtn.classList.add("dark-mode-toggle");
-//     localStorage.setItem("lightmode", null);
-// };
-
-// if (lightmode === "enabled") {
-//   enableLightMode(); // set state of lightmode on page load
-// }
-
-// modeBtn.addEventListener("click", (e) => {
-//   lightmode = localStorage.getItem("lightmode"); // update lightmode when clicked
-//     if (lightmode === "disabled") {
-//     enableLightMode();
-//     } else {
-//     disableLightMode();
-//     }
-// });
-
-
-
-
-// Funktion för att hämta och visa JSON-data
-function fetchEducation() {
-    // Använd fetch() för att hämta JSON-filen
-    fetch("educationdata.json")
-        .then(function (response) {
-            // Konvertera JSON-texten till ett JavaScript-objekt
+function education() {
+    fetch("./education.json")
+        .then(function(response) {
             return response.json();
         })
-        .then(function (data) {
+        .then(function(data) {
             // Uppdatera HTML med data från JSON
-            document.getElementById("#title").innerText = data.title;
-            document.getElementById("#description").innerText = data.description;
+            document.getElementById("edTitle").innerText = data.title;
 
             // Hitta listan i HTML
-            let itemList = document.getElementById("#filelist");
-            itemList.innerHTML = ""; // Rensa listan först
+            let itemList = document.getElementById("edFilelist");
+            itemList.innerHTML = "";
 
-
-            // Loopa igenom JSON-listan och skapa ett HTML-element för varje objekt
-            data.items.forEach(function (item) {
-                let li = document.createElement("li"); // Skapa ett nytt <li>-element
-                li.textContent = item.program + " - " + item.school + " - " + item.year; // Sätt textinnehåll
-                itemList.appendChild(li); // Lägg till i listan
+            data.items.forEach(function(item) {
+                let li = document.createElement("li"); 
+                li.textContent = `${item.school} | ${item.program} , ${item.duration} hej\n ${item.description}`;
+                itemList.appendChild(li);
+                console.log(li.textContent);
             });
         })
-        .catch(function (error) {
+        .catch(function(error) {
             console.error("Fel vid hämtning av JSON:", error);
         });
 }
 
-// Kör funktionen när sidan laddas
-fetchEducation();
+education();
 
 
-// Funktion för att hämta och visa JSON-data
-function fetchExperience() {
-    // Använd fetch() för att hämta JSON-filen
-    fetch("experiencedata.json")
-        .then(function (response) {
-            // Konvertera JSON-texten till ett JavaScript-objekt
+function experience() {
+    fetch("./experience.json")
+        .then(function(response) {
             return response.json();
         })
-        .then(function (data) {
+        .then(function(data) {
             // Uppdatera HTML med data från JSON
-            document.getElementById("#title2").innerText = data.title;
-            document.getElementById("#description2").innerText = data.description;
+            document.getElementById("exTitle").innerText = data.title;
 
             // Hitta listan i HTML
-            let itemList = document.getElementById("#filelist2");
+            let itemList = document.getElementById("exFilelist");
             itemList.innerHTML = ""; // Rensa listan först
 
-
-            // Loopa igenom JSON-listan och skapa ett HTML-element för varje objekt
-            data.items.forEach(function (item) {
-                let li = document.createElement("li"); // Skapa ett nytt <li>-element
-                li.textContent = item.company + " - " + item.role + " - " + item.year; // Sätt textinnehåll
-                itemList.appendChild(li); // Lägg till i listan
+            data.items.forEach(function(item) {
+                let li = document.createElement("li"); 
+                li.textContent = `${item.company} | ${item.title} , ${item.duration} - ${item.description}`;
+                itemList.appendChild(li);
             });
         })
-        .catch(function (error) {
+        .catch(function(error) {
             console.error("Fel vid hämtning av JSON:", error);
         });
 }
 
-// Kör funktionen när sidan laddas
-fetchExperience();
+console.log(experience);
+experience();
 
 
-// function fetchData() {
-//     // Använd fetch() för att hämta JSON-filen
-//     fetch("experiencedata.json")
-//         .then(function(response) {
-//             // Konvertera JSON-texten till ett JavaScript-objekt
-//             return response.json();
-//         })
-//         .then(function(data) {
-//             // Uppdatera HTML med data från JSON
-//             document.getElementById("title2").innerText = data.title;
-//             document.getElementById("description2").innerText = data.description;
 
-//             // Hitta listan i HTML
-//             var itemList = document.getElementById("filelist2");
-//             itemList.innerHTML = ""; // Rensa listan först
+// Modals lånade och inspirerade av W3 Schools https://www.w3schools.com/howto/howto_css_modals.asp
 
-//             // Loopa igenom JSON-listan och skapa ett HTML-element för varje objekt
-//             data.items.forEach(function(item) {
-//                 var li = document.createElement("li"); // Skapa ett nytt <li>-element
-//                 li.textContent = item.company + " - " + item.title + " - " + item.year; // Sätt textinnehåll
-//                 itemList.appendChild(li); // Lägg till i listan
-//             });
-//         })
-//         .catch(function(error) {
-//             console.error("Fel vid hämtning av JSON:", error);
-//         });
-// }
+const modal1 = document.getElementById("Modal1");
+const modal2 = document.getElementById("Modal2");
+const modal3 = document.getElementById("Modal3");
+const modal4 = document.getElementById("Modal4");
 
-// // Kör funktionen när sidan laddas
-// fetchData();
+const btn1 = document.getElementById("modalBtn1");
+const btn2 = document.getElementById("modalBtn2");
+const btn3 = document.getElementById("modalBtn3");
+const btn4 = document.getElementById("modalBtn4");
+
+const span1 = document.getElementsByClassName("close1")[0];
+const span2 = document.getElementsByClassName("close2")[0];
+const span3 = document.getElementsByClassName("close3")[0];
+const span4 = document.getElementsByClassName("close4")[0];
+
+// Open the modal 
+btn1.onclick = function () {
+    modal1.style.display = "block";
+}
+
+btn2.onclick = function () {
+    modal2.style.display = "block";
+}
+
+btn3.onclick = function () {
+    modal3.style.display = "block";
+}
+
+btn4.onclick = function () {
+    modal4.style.display = "block";
+}
+
+// Close the modal
+span1.onclick = function () {
+    modal1.style.display = "none";
+}
+
+span2.onclick = function () {
+    modal2.style.display = "none";
+}
+span3.onclick = function () {
+    modal3.style.display = "none";
+}
+span4.onclick = function () {
+    modal4.style.display = "none";
+}
+
+
